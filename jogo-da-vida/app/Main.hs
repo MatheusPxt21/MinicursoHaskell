41581830f2571f15b1f896a209a2989fcfd5b617
MatheusPxt21 <mateusnascimentopxt@gmail.com>
2023-09-29 13:19:28 -0300

    .#

--- jogo-da-vida/app/Main.hs
+++ jogo-da-vida/app/Main.hs
@@ -1,4 +1,16 @@
 module Main where
 
+import Data.Matrix (Matrix, fromLists, prettyMatrix)
+
+lerInicial :: String -> IO (Matrix Bool)
+lerInicial fp = do
+    texto <- readFile fp 
+    let linhas = lines texto
+    let booleanos = map (map (== '#')) linhas
+    return(fromLists booleanos)
+
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+    let m = fromLists [[1,2], [3,4]]
+    putStrLn (prettyMatrix m)
