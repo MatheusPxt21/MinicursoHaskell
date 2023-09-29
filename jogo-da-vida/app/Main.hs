02a99bd3727f952ec1a64944a8807232e49cff8e
MatheusPxt21 <mateusnascimentopxt@gmail.com>
2023-09-29 14:00:23 -0300

    jogoDaVida rodando

--- jogo-da-vida/app/Main.hs
+++ jogo-da-vida/app/Main.hs
@@ -2,6 +2,7 @@ module Main where
 
 import           Data.Matrix (Matrix, fromLists, prettyMatrix)
 import qualified Data.Matrix as M 
+import           Data.Maybe (fromMaybe)
 
 lerInicial :: String -> IO (Matrix Bool)
 lerInicial fp = do
@@ -11,18 +12,25 @@ lerInicial fp = do
     return(fromLists booleanos)
 
 atualizar :: Matrix Bool -> Matrix Bool
-atualizar m = M.mapPos () m
+atualizar m = M.mapPos (atualizarCelula m) m
 
 atualizarCelula :: Matrix Bool -> (Int, Int) -> Bool -> Bool
-atualizarCelula m (i, j) v = v
+atualizarCelula m (i, j) v = (v && (vivos == 2 || vivos == 3)) || vivos == 3
     where
         deslocamentos = [(-1, -1), (-1, 0), (-1, 1),
                          (0, -1),           (0, 1),
                          (1, 1),   (1, 0),  (1, 1)]
         vizinhos = map (\(di,dj) -> M.safeGet (i+di) (j+dj) m) deslocamentos
+        vizinhos' = map (fromMaybe False) vizinhos
+        vivos = length (filter (\x -> x) vizinhos')
 
 
 main :: IO ()
 main = do
-    let m = fromLists [[1,2], [3,4]]
-    putStrLn (prettyMatrix m)
+    let fp = "teste.txt"
+    m <- lerInicial fp
+    run m
+    where 
+        run m = do
+            putStrLn (prettyMatrix m)        
+            run (atualizar m)
